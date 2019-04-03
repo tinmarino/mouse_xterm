c50752a062107a4022210409469a9ddbcc250723
tinmarino <tinmarino@gmail.com>
2019-04-03 13:36:56 -0300

    Bahs with a .conf for bindings + Remove readings keys when get cursor location

--- mouse.sh
+++ mouse.sh
@@ -20,6 +20,7 @@
     (at your option) any later version.
 END_DOC
 
+source $(dirname $)/variable.sh
 
 # Mouse track status : 1 tracking; 0 Not tracking
 mouse_track_status=0
@@ -28,20 +29,20 @@ mouse_track_status=0
 function mouse_track_log {
   # Log for debug
   :
-  # echo $1 >> /tmp/xterm_monitor
+  echo $1 >> /tmp/xterm_monitor
 }
 
 
 function mouse_track_echo_enable {
   # Enable (high)
-  echo -ne "\e[?1000;1006;1015h"
+  echo -ne $s_echo_enable
   mouse_track_status=1
 }
 
 
 function mouse_track_echo_disable {
   # Disable (low)
-  echo -ne "\e[?1000;1006;1015l"
+  echo -ne $s_echo_disable
   mouse_track_status=0
 }
 
@@ -50,9 +51,9 @@ function mouse_track_read_keys_remaining {
   # Read $keys <- Stdin (until 'm')
   mouse_track_log "---------------"
   keys=""
-  while  read -n 1 c; do
+  while read -n 1 c; do
     keys="$keys$c"
-    [[ $c == 'M' || $c == 'm' ]] && break
+    [[ $c == 'M' || $c == 'm' || $c == 'R' || $c == '' ]] && break
   done
   mouse_track_log "keys = $keys"
 }
@@ -61,10 +62,11 @@ function mouse_track_read_keys_remaining {
 function mouse_track_read_cursor_pos {
   # Read $cursor_pos <- xterm <- readline
   # Clean stdin
-  mouse_track_read_keys_remaining
+  # TODO recover
+  # mouse_track_read_keys_remaining
 
   # Ask cursor pos
-  echo -en "\E[6n"
+  echo -en $s_echo_get_cursor_pos
 
   # Read it
   read -sdR cursor_pos
@@ -83,7 +85,6 @@ function mouse_track_trap_disable_mouse {
   # Leave for some commands
   [ -n "$COMP_LINE" ] && return  # do nothing if completing
   [ "$BASH_COMMAND" = "$PROMPT_COMMAND" ] && return # don't cause a preexec for $PROMPT_COMMAND
-  [[ "$BASH_COMMAND" =~ \s*ipy.* ]] && return # ipython : can keep bindings
 
   # Disable mouse as callback
   mouse_track_log "trap : Stoping mouse tracking"
@@ -92,27 +93,31 @@ function mouse_track_trap_disable_mouse {
 
 
 function mouse_track_0_cb {
-  local x0 y0 x1 y1 xy col line_pos
   # Callback for mouse button 0 click/release
+  local x0 y0 x1 y1 xy col line_pos
+
   # Read rest
   mouse_track_read_keys_remaining
+  mouse_track_log "Mouse click with $keys"
+
+  # Get click X,y
   xy=${keys:0:-1}
   let x1=${xy%%;*}
   let y1=${xy##*;}
+  mouse_track_log "x1 = $x1 && y1 = $y1"
 
   # Get mouse position (bol)
   mouse_track_read_cursor_pos
   x0=${cursor_pos##*;}
   y0=${cursor_pos%%;*}
+  mouse_track_log "x0 = $x0 && y0 = $y0 && cursor_pos = $cursor_pos"
 
   # Calculate line position
   let col=$y1-$y0
   [[ col -lt 0 ]] && let col=0
   let col=$col*$COLUMNS
   let line_pos="$x1 - $x0 - 2 + $col"
-  mouse_track_log "x1 = $x1 && y1 = $y1 && line_pos = $line_pos"
-  mouse_track_log "x0 = $x0 && y0 = $y0 && cursor_pos = $cursor_pos"
-  mouse_track_log "col = $col"
+  mouse_track_log "col = $col && line_pos = line_pos"
   # TODO if too low, put on last line
 
   # Move cursor
@@ -131,21 +136,21 @@ function mouse_track_void_cb {
 
 
 function mouse_track_bindings {
-  # Bindings TODO removable
-  # Utils
-  bind    '"\C-91": clear-screen'
-  bind -x '"\C-92": printf "\e[?1000;1006;1015h"'
-  bind -x '"\C-91": printf "\e[?1000;1006;1015l"'
-  bind    '"\C-98": beginning-of-line'
-  bind -x '"\C-99": mouse_track_0_cb'
+  # Binds 
+  i=($s_bind_1 $s_bind_2 $s_bind_3 $s_bind_4)
+  j=($s_macro_1 $s_macro_2 $s_macro_3 $s_macro_4)
+  for (( k=0; k<${#i[@]}; k++ )) ; do
+    mouse_track_log "binding ${i[k]} -> ${j[k]}"
+    bind \"${i[k]}\":${j[k]}
+  done
 
-  # Bind Click
-  bind '"\e[<0;": "\C-98\C-99"'
-  bind -x '"\e[<64;": mouse_track_void_cb'
-  bind -x '"\e[<65;": mouse_track_void_cb'
-
-  # Bind C-l to reenable mouse
-  bind '"\C-l": "\C-91\C-92"'
+  # Bind -X
+  i=($s_bindx_1 $s_bindx_2 $s_bindx_3 $s_bindx_4)
+  j=($s_macrox_1 $s_macrox_2 $s_macrox_3 $s_macrox_4)
+  for (( k=0; k<${#i[@]}; k++ )) ; do
+    mouse_track_log "binding -x ${i[k]} -> ${j[k]}"
+    bind -x \"${i[k]}\":${j[k]}
+  done
 }
 
 
