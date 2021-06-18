60d7e20e61fb2b6f6b71b81056aaace6c9d8cc45
Tinmarino <tinmarino@gmail.com>
2021-06-18 18:40:53 -0400

    Embed "data" in code, escape codes close to the bind command

--- mouse.sh
+++ mouse.sh
@@ -27,41 +27,9 @@ g_mouse_track_status=0
 
 
 # Escape sequences
-s_echo_enable='\e[?1000;1006;1015h'
-s_echo_disable='\e[?1000;1006;1015l'
-s_echo_get_cursor_pos='\e[6n'
-
-# Bindins
-## 91 Clear
-s_bind_1='\C-91'
-s_macro_1=clear-screen
-
-## 93 Beginning of line
-s_bind_2='\C-93'
-s_macro_2=beginning-of-line
-
-## 92 Disable (X)
-s_bindx_1='\C-92'
-s_macrox_1=mouse_track_echo_disable
-
-## 94 Click (X)
-s_bindx_2='\C-94'
-s_macrox_2=mouse_track_cb_0
-
-
-## Click (0) Begining of line + X click cb
-## TODO remove beginning of line
-s_bind_3='\e[<0;'
-s_macro_3='"\C-93\C-94"'
-
-## Scrool up
-s_bindx_3='\e[<64;'
-s_macrox_3=mouse_track_cb_scroll_up
-
-## Scrool down
-s_bindx_4='\e[<65;'
-s_macrox_4=mouse_track_cb_scroll_down
-
+s_echo_enable='\033[?1000;1006;1015h'
+s_echo_disable='\033[?1000;1006;1015l'
+s_echo_get_cursor_pos='\033[6n'
 
 mouse_track_log() {
   # Log for debug
@@ -97,7 +65,6 @@ mouse_track_read_keys_remaining() {
   done
   mouse_track_log "g_keys = $g_keys"
 }
-
 
 mouse_track_read_cursor_pos() {
   # Read $cursor_pos <- xterm <- readline
@@ -139,7 +106,7 @@ mouse_track_trap_disable_mouse() {
 }
 
 
-mouse_track_cb_0() {
+mouse_track_cb_click() {
   # Callback for mouse button 0 click/release
   local x0 y0 x1 y1 col line_pos
 
@@ -194,7 +161,7 @@ mouse_track_cb_scroll_up() {
     tmux copy-mode -e
     return
   fi
-      
+
   mouse_track_log 'Cb: Scroll Up -> echo esc'
   printf "%b" "$s_echo_enable$s_bindx_3$g_keys"
 }
@@ -206,28 +173,33 @@ mouse_track_cb_scroll_down() {
 }
 
 
-mouse_track_bindings() {
-  # Binds 
-  i=("$s_bind_1" "$s_bind_2" "$s_bind_3")
-  j=("$s_macro_1" "$s_macro_2" "$s_macro_3")
-  for (( k=0; k<${#i[@]}; k++ )) ; do
-    mouse_track_log "binding ${i[k]} -> ${j[k]}"
-    bind "\"${i[k]}\":${j[k]}"
-  done
+mouse_track_set_bindings() {
+  # Bindings
 
-  # Bind -X
-  i=("$s_bindx_1" "$s_bindx_2" "$s_bindx_3" "$s_bindx_4")
-  j=("$s_macrox_1" "$s_macrox_2" "$s_macrox_3" "$s_macrox_4")
-  for (( k=0; k<${#i[@]}; k++ )) ; do
-    mouse_track_log "binding -x ${i[k]} -> ${j[k]}"
-    bind -x "\"${i[k]}\":${j[k]}"
-  done
+  mouse_track_log "binding: 93 -> Beginning of line (Readline)"
+  local s_bind_to_bol='\C-93'
+  bind "\"$s_bind_to_bol\":begining-of-line"
+
+  mouse_track_log "binding: 94 -> Click (eXecute)"
+  local s_bindx_to_click='\C-94'
+  bind -x "\"$s_bindx_to_click\":mouse_track_cb_click"
+
+  mouse_track_log 'binding: Scrool up (eXecute)'
+  bind -x "\"\033[<64\":mouse_track_cb_scroll_up"
+
+  mouse_track_log 'binding: Scrool down (eXecute)'
+  bind -x "\"\033[<65;\":mouse_track_cb_scroll_down"
+
+  mouse_track_log 'binding: Click -> 93 + 94 (Readline)'
+  ## Click (0) Begining of line + X click cb
+  ## TODO remove beginning of line
+  bind "\"\033[<0;\":\"$s_bind_to_beginning_of_line$s_bindx_to_click\""
 }
 
 
 mouse_track_start() {
   # Init : Enable mouse tracking
-  mouse_track_bindings
+  mouse_track_set_bindings
 
   # Disable mouse tracking before each command
   trap 'mouse_track_trap_disable_mouse' DEBUG
