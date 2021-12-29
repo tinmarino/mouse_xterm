af4fa3bb3b30c4c3b4d76ba0bcfde77ae0ce67ab
Tinmarino <tinmarino@gmail.com>
2021-12-29 01:21:42 -0300

    Lint: start to use typed globals

--- mouse.sh
+++ mouse.sh
@@ -20,12 +20,14 @@
     (at your option) any later version.
 END_DOC
 
+set -u
+
 # Keystrokes <string>: usually output by the terminal
-g_key=''
-# Mouse track status <int>: 1 tracking; 0 Not tracking
-g_mouse_track_status=0
+declare -g g_key=''
+# Mouse track status <bool>: 1 tracking; 0 Not tracking
+declare -gi gb_mouse_track_status=0
 # Binding <dict>: seq -> bash function
-declare -A g_binding=(
+declare -gA gd_binding=(
   [<64;]=mouse_track_cb_scroll_up
   [<65;]=mouse_track_cb_scroll_down
   ## Click (0) Begining of line + X click cb
@@ -57,14 +59,15 @@ mouse_track_cb_drag1() { mouse_track_tmux_proxy 'tmux copy-mode -e \; send-keys 
 
 
 # Cursor position <string>: 50;1 (x;y) if click on line 1, column 50: starting at 1;1
-g_cursor_pos='1;1'
+declare -gi gi_cursor_x=0
+declare -gi gi_cursor_y=0
 # Tmux command to launch
-g_tmux_cmd=''
+declare -g g_tmux_cmd=''
 
 # Escape sequences
-s_echo_enable='\033[?1000;1002;1006;1015h'
-s_echo_disable='\033[?1000;1002;1006;1015l'
-s_echo_get_cursor_pos='\033[6n'
+declare -g gs_echo_enable=$'\033[?1000;1002;1006;1015h'
+declare -g gs_echo_disable=$'\033[?1000;1002;1006;1015l'
+declare -g gs_echo_get_cursor_pos=$'\033[6n'
 
 mouse_track_log() {
   # Log for debug
@@ -74,15 +77,15 @@ mouse_track_log() {
 
 mouse_track_echo_enable() {
   # Enable (high)
-  printf "%b" "$s_echo_enable"
-  g_mouse_track_status=1
+  printf "%b" "$gs_echo_enable"
+  gb_mouse_track_status=1
 }
 
 
 mouse_track_echo_disable() {
   # Disable (low)
-  printf "%b" "$s_echo_disable"
-  g_mouse_track_status=0
+  printf "%b" "$gs_echo_disable"
+  gb_mouse_track_status=0
 }
 
 
@@ -95,7 +98,8 @@ mouse_track_read_keys_remaining() {
   while read -rt 0.001 -n 1 c; do
     mouse_track_log "reading $c"
     g_key="$g_key$c"
-    [[ $c == 'M' || $c == 'm' || $c == 'R' || $c == '' ]] && break
+    # M and m for click, R for get_cursor_pos
+    [[ $c == M || $c == m || $c == R || $c == '' ]] && break
   done
   mouse_track_log "g_key = $g_key"
 }
@@ -103,17 +107,38 @@ mouse_track_read_keys_remaining() {
 mouse_track_read_cursor_pos() {
   # Read $cursor_pos <- xterm <- readline
   # Out: 50;1 (x;y) if click on line 1, column 50: starting at 1;1
+  # See: https://unix.stackexchange.com/questions/88296/get-vertical-cursor-position
+  local row=0 col=0
 
   # Clean stdin
   mouse_track_read_keys_remaining
 
-  # Ask cursor pos
-  printf "%b" "$s_echo_get_cursor_pos"
 
-  # Read it
-  read -srdR g_cursor_pos
-  g_cursor_pos=${g_cursor_pos#*[}
-  mouse_track_log "cursor_pos returns:  $g_cursor_pos"
+  ## Read it
+  #read -srdR g_cursor_pos
+  #g_cursor_pos=${g_cursor_pos#*[}
+  #mouse_track_log "cursor_pos returns:  $g_cursor_pos"
+  #mouse_track_log "cursor_pos pre"
+  #{
+    #exec < /dev/tty
+    #oldstty=$(stty -g)
+    #stty raw -echo min 0
+    # Ask cursor pos
+    #printf "%b" "$gs_echo_get_cursor_pos" > /dev/tty
+    #read -r -d R -p $'\E[6n' -a pos
+    #IFS=';' read -r -dR -p $'\e[6n' row col
+    IFS=';' read -r -dR -p "$gs_echo_get_cursor_pos" row col
+    #stty "$oldstty"
+  #}
+  row=${row#*[}
+
+  # Parse it
+  (( gi_cursor_x = col ))
+  (( gi_cursor_y = row ))
+  #(( gi_cursor_x = ${g_cursor_pos##*;} ))
+  #(( gi_cursor_y = ${g_cursor_pos%%;*} ))
+
+  mouse_track_log "cursor_pos returns:  $gi_cursor_x, $gi_cursor_y"
 }
 
 
@@ -122,12 +147,12 @@ mouse_track_trap_disable_mouse() {
   # Callback : traped to debug : Disable XTERM escape
 
   # log
-  mouse_track_log "trap ($g_mouse_track_status) for : $BASH_COMMAND"
+  mouse_track_log "trap ($gb_mouse_track_status) for : $BASH_COMMAND"
 
   # Clauses: leave if ...
   # -- mouse track disabled yet
-  [[ $g_mouse_track_status == 0 ]] \
-      || [[ -n "$COMP_LINE" ]] \
+  [[ $gb_mouse_track_status == 0 ]] \
+      || [[ -v COMP_LINE && -n "$COMP_LINE" ]] \
       || [[ "$BASH_COMMAND" == "$PROMPT_COMMAND" ]] \
       || [[ "$BASH_COMMAND" =~ ^mouse_track* ]] \
       && { mouse_track_log "trap disregarded (clause)"; return; }
@@ -143,16 +168,17 @@ mouse_track_trap_disable_mouse() {
 
 mouse_track_cb_click() {
   # Callback for mouse button 0 click/release
-  local x0 y0 x1 y1 col readline_point
+  local x1 y1 col readline_point
 
   # Read rest
   mouse_track_read_keys_remaining
   mouse_track_log "---------------- Mouse click with $g_key"
 
   # Log readline pre value
-  mouse_track_log "$(echo "Readline pre: $READLINE_POINT, $READLINE_LINE, $READLINE_MARK" | xxd)"
+  mouse_track_log "Readline point, line, mark..."
+  mouse_track_log "$(echo "$READLINE_POINT, $READLINE_LINE, $READLINE_MARK" | xxd)"
 
-  # Only work for M
+  # Clause: Only work for M
   local mode=${g_key: -1}
   [[ "$mode" == m ]] && {
     mouse_track_log "Release ignored"
@@ -163,23 +189,21 @@ mouse_track_cb_click() {
   local xy=${g_key:0:-1}
   local x1=${xy%%;*}
   local y1=${xy##*;}
+  mouse_track_log "x1 = $x1 && y1 = $y1"
 
   # Get Cursor position (x0, y0)
   mouse_track_read_cursor_pos
-  (( x0=${g_cursor_pos##*;} ))
-  (( y0=${g_cursor_pos%%;*} ))
-  mouse_track_log "x0 = $x0 && y0 = $y0 && g_cursor_pos = $g_cursor_pos"
-  mouse_track_log "x1 = $x1 && y1 = $y1"
+  mouse_track_log "x0 = $gi_cursor_x && y0 = $gi_cursor_y"
 
   # Calculate line position
-  (( col = y1 - y0 ))
+  (( col = y1 - gi_cursor_y ))
   (( col < 0 )) && (( col = 0 ))
   readarray -t a_line <<< "$READLINE_LINE"
   for i in "${a_line[@]}"
   do
       mouse_track_log "Array line: $i"
   done
-  (( readline_point = x1 - x0 - 2 + col * COLUMNS ))
+  (( readline_point = x1 - gi_cursor_x - 2 + col * COLUMNS ))
   mouse_track_log "col = $col && readline_point = $readline_point"
   # TODO if too low, put on last line
 
@@ -235,24 +259,27 @@ mouse_track_tmux_proxy() {
 mouse_track_cb_scroll_down() {
   mouse_track_log 'Cb: Scroll Down'
   mouse_track_read_keys_remaining
-  #printf "%b" "$s_echo_enable$s_bindx_4$g_key"
+  #printf "%b" "$gs_echo_enable$s_bindx_4$g_key"
 }
 
 
 mouse_track_set_bindings() {
+  local s_keyseq=''
   mouse_track_log 'Set bindings'
-  for s_keyseq in "${!g_binding[@]}"; do
-    local s_fct=${g_binding[$s_keyseq]}
+  for s_keyseq in "${!gd_binding[@]}"; do
+    mouse_track_log "keyset: $s_keyseq"
+    local s_fct=${gd_binding[$s_keyseq]}
     bind -x "\"\033[$s_keyseq\":$s_fct"
   done
 }
 
 
 mouse_track_unset_bindings() {
+  local s_keyseq=''
   # Unset mouse event callback binding
   mouse_track_log 'Unset bindings'
-  for s_keyseq in "${!g_binding[@]}"; do
-    local s_fct=${g_binding[s_keyseq]}
+  for s_keyseq in "${!gd_binding[@]}"; do
+    local s_fct=${gd_binding[$s_keyseq]}
     bind -r "$s_keyseq"
   done
 }
